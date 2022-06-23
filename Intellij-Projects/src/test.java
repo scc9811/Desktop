34a565d5777ed0d69c6fcf3efe117ab596bbb10f
k s jeon <wjsrudtjr98@naver.com>
2022-06-23 21:10:38 +0900

    BEAK JOON

--- Intellij-Projects/src/test.java
+++ Intellij-Projects/src/test.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
 public class test {
     public static void main(String[] args){
-        System.out.println("hell0");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int fiveCount = 0;
+        int threeCount = 0;
+        input.close();
+
+        fiveCount = n/5;
+        if (n%5 ==0) System.out.println(fiveCount);
+        else{
+
+            boolean B = false;
+            while(fiveCount>=0){
+                threeCount = (n - fiveCount*5)/3;
+                if ( (fiveCount*5 + threeCount*3) == n ){
+                    System.out.println(fiveCount+threeCount);
+                    B = true;
+                    break;
+                }
+                fiveCount--;
 
 
+            }
+            if (B==false) System.out.println(-1);
+
+        }
     }
 }

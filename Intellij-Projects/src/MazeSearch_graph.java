0f5009b904767c34b4f16b1a4a3f10c95a2cd5ba
k s jeon <wjsrudtjr98@naver.com>
2022-07-26 09:01:57 +0900

    MazeSearch_graph

--- Intellij-Projects/src/MazeSearch_graph.java
+++ Intellij-Projects/src/MazeSearch_graph.java
@@ -5,10 +5,9 @@ import java.util.*;
 
 public class MazeSearch_graph {
     public static void main(String[] args)throws IOException {
-        Stack<int[]> stack = new Stack<>();
+        Queue<int[]> queue = new LinkedList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] array = new int[N][M];
@@ -19,22 +18,56 @@ public class MazeSearch_graph {
                 array[i][j] = Integer.parseInt(s.substring(j,j+1));
             }
         }
-        int currentI = 0;
-        int currentJ = 0;
-        int count = 0;
 
-        int[] current = {currentI,currentJ,count};
+        int count =0;
 
-        stack.add(current);
+        queue.add(new int[] {0,0});
+        array[0][0] = 0;
+        count++;
+        int tmp = 1;
+        while(true){
+            int[] semiArray = queue.poll();
+            int i=semiArray[0];
+            int j=semiArray[1];
+            if (i==N-1 && j==M-1) break;
+
+            if (i+1<N && array[i+1][j]==1){
+                queue.add(new int[] {i+1,j});
+                array[i+1][j] = 0;
+            }
+            if (i>0 && array[i-1][j]==1){
+                queue.add(new int[] {i-1,j});
+                array[i-1][j] = 0;
+            }
+            if (j+1<M && array[i][j+1]==1){
+                queue.add(new int[] {i,j+1});
+                array[i][j+1] = 0;
+            }
+            if (j>0 && array[i][j-1]==1){
+                queue.add(new int[] {i,j-1});
+                array[i][j-1] = 0;
+            }
+            tmp--;
+            if (tmp==0){
+                tmp = queue.size();
+                count++;
+            }
 
 
 
 
 
 
-//        for (int i=0; i<N; i++){
-//            System.out.println(Arrays.toString(array[i]));
-//        }
+        }
+        System.out.println(count);
+
+
+
+
+
+
+
+
 
 
 

ba99fd3654a42a05f0fad294cc03956e38d82522
k s jeon <wjsrudtjr98@naver.com>
2022-07-29 23:20:34 +0900

    Goldbach_SOSU_fixed

--- Intellij-Projects/src/GoldBach.java
+++ Intellij-Projects/src/GoldBach.java
@@ -1,43 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class GoldBach {
     static boolean[] array = new boolean[10001];
     public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        int max = 3;
         array[2] = true;
-        while (testCase-->0){
-            int n = Integer.parseInt(br.readLine());
-            if (n>max){
-                for (int i=max; i<=n; i++){
-                    boolean tf = true;
-                    for (int j=2; j*j<=i; j++){
-                        if (i%j==0){
-                            tf = false;
-                            break;
-                        }
-                    }
-                    if (tf) array[i]=true;
-                    else array[i] = false;
+        for (int i=3; i<=10000; i++){
+            boolean tf = true;
+            for (int j=2; j*j<=i; j++){
+                if(i%j==0){
+                    tf = false;
+                    break;
                 }
             }
+            if (tf) array[i] = true;
+            else array[i] = false;
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(br.readLine());
+
+
+        while (testCase-->0){
+            StringBuilder sb = new StringBuilder();
+            int n = Integer.parseInt(br.readLine());
             for (int i=n/2; i<n; i++){
                 if (array[i] && array[n-i]){
-                    System.out.println(n-i+" "+i);
+                    sb.append(n-i).append(" ").append(i).append("\n");
+                    bw.write(sb.toString());
                     break;
                 }
             }
 
 
-
-
-
-
-
         }
+        bw.close();
 
 
 
